25b2cc4396f6a77154a6b8563be2c0fa5945ec12
Sharonanota <matakwamatakwa@gmail.com>
2022-04-07 08:28:45 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import time
+import random
+gamelist = ['You lose the race', 'You win the race']
+Player = input('Sharon')
+print(' Hi ' + Player + ' , welcome to the running race game')
+time.sleep(2)
+print('4')
+time.sleep(2)
+print('3')
+time.sleep(1)
+print('2')
+time.sleep(1)
+print('1')
+time.sleep(2)
+print(random.choice(gamelist))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
